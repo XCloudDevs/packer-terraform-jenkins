527a4b598978c19f2f48fe206eed037418d64911
Adenusi Damilola <adenusidamilola5@gmail.com>
2022-02-01 16:27:36 +0100

    made some changes

--- output.tf
+++ output.tf
@@ -6,9 +6,6 @@ output "app-ip" {
   value = [aws_instance.app-instance.*.public_ip]
 }
 
-output "s3-bucket" {
-  value = aws_s3_bucket.terraform-state.bucket
-}
 
 # sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 # ps aux | grep jenkins
--- s3.tf
+++ s3.tf
@@ -1,15 +1,15 @@
-resource "aws_s3_bucket" "terraform-state" {
-  bucket = "terraform-state-${random_string.random.result}"
-  acl    = "private"
+# resource "aws_s3_bucket" "terraform-state" {
+#   bucket = "terraform-state-${random_string.random.result}"
+#   acl    = "private"
 
-  tags = {
-    Name = "Terraform state"
-  }
-}
+#   tags = {
+#     Name = "Terraform state"
+#   }
+# }
 
-resource "random_string" "random" {
-  length  = 8
-  special = false
-  upper   = false
-}
+# resource "random_string" "random" {
+#   length  = 8
+#   special = false
+#   upper   = false
+# }
 
